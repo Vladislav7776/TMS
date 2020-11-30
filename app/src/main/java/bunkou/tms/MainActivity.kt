4bcea342952a043726e4c5e294a6636ef8ab77c9
Vladislav7776 <vladvlad7776>
2020-11-30 21:44:59 +0300

    Add-Activity

--- app/src/main/java/bunkou/tms/MainActivity.kt
+++ app/src/main/java/bunkou/tms/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import bunkou.tms.HomeWork10.MainActivityNotification
 import bunkou.tms.HomeWork11.MainWeather
 import bunkou.tms.HomeWork11.WeatherAppWidgetConfigureActivity
+import bunkou.tms.HomeWork12.MainLight
 import bunkou.tms.HomeWork2.MainActivity2
 import bunkou.tms.HomeWork3.MainActivity3
 import bunkou.tms.HomeWork5.Coffe
@@ -35,6 +36,7 @@ class MainActivity : AppCompatActivity() {
             R.id.button8 -> Intent(this, MainRetrofitActivity::class.java)
             R.id.button9 -> Intent(this, MainActivityNotification::class.java)
             R.id.button10 -> Intent(this, MainWeather::class.java)
+            R.id.button11 -> Intent(this, MainLight::class.java)
             else -> null
         }
         if (myIntent != null) {
